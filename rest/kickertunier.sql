eb54557459d906bf9c54163f59becf48d1f864bb
Stefan Chrobak <info@netcodev.de>
2014-11-03 18:02:57 +0100

    set dump reset

--- rest/kickertunier.sql
+++ rest/kickertunier.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 21. Okt 2014 um 21:34
+-- Erstellungszeit: 02. Nov 2014 um 22:12
 -- Server Version: 5.5.27
 -- PHP-Version: 5.4.7
 
@@ -26,16 +26,19 @@ SET time_zone = "+00:00";
 -- Tabellenstruktur für Tabelle `games`
 --
 
+DROP TABLE IF EXISTS `games`;
 CREATE TABLE IF NOT EXISTS `games` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `team_1` int(11) NOT NULL,
   `team_2` int(11) NOT NULL,
   `result` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
   `winner_id` int(11) NOT NULL,
+  `tournaments_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `team_1` (`team_1`,`team_2`),
-  KEY `winner_id` (`winner_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  KEY `team1` (`team_1`,`team_2`),
+  KEY `winner_id` (`winner_id`),
+  KEY `tournaments_id` (`tournaments_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=16 ;
 
 -- --------------------------------------------------------
 
@@ -55,22 +58,31 @@ CREATE TABLE IF NOT EXISTS `tournaments` (
 -- Tabellenstruktur für Tabelle `teams`
 --
 
+DROP TABLE IF EXISTS `teams`;
 CREATE TABLE IF NOT EXISTS `teams` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `player_1` int(11) NOT NULL,
   `player_2` int(11) NOT NULL,
   `teamname` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `tournaments_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `player_1` (`player_1`,`player_2`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=42 ;
+  KEY `user_1_id` (`player_1`,`player_2`),
+  KEY `tournaments_id` (`tournaments_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=50 ;
+
+-- --------------------------------------------------------
 
 --
--- Daten für Tabelle `teams`
+-- Tabellenstruktur für Tabelle `tournaments`
 --
 
-INSERT INTO `teams` (`id`, `user_1_id`, `user_2_id`, `teamname`) VALUES
-(40, 56, 57, 'Musterteam'),
-(41, 58, 59, 'Vollenergie');
+DROP TABLE IF EXISTS `tournaments`;
+CREATE TABLE IF NOT EXISTS `tournaments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -78,23 +90,16 @@ INSERT INTO `teams` (`id`, `user_1_id`, `user_2_id`, `teamname`) VALUES
 -- Tabellenstruktur für Tabelle `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `secondname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `nickname` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=60 ;
-
---
--- Daten für Tabelle `users`
---
-
-INSERT INTO `users` (`id`, `firstname`, `secondname`, `nickname`) VALUES
-(56, 'Paula', 'Vollwert', 'PaulaV'),
-(57, 'Paul', 'Vollwert', 'PaulV'),
-(58, 'Ernst', 'Mustermann', 'ErnstM'),
-(59, 'Susi', 'Musterfrau', 'SusiM');
+  `tournaments_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tournaments_id` (`tournaments_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=73 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
