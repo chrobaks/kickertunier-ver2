1710c604857f7e471c5fc214bcbffc8725cc5a88
Stefan Chrobak <info@netcodev.de>
2014-11-05 12:04:29 +0100

    set require config

--- js/app/module/mainApp.js
+++ js/app/module/mainApp.js
@@ -1,6 +1,19 @@
-(function() {
+define([
+    'angular',
+    'uiRouter',
+    'ngResource',
+    'filter/mainAppFilter',
+    'factory/mainAppFactory'
+],function ( angular ) {
+
     "use strict";
 
-    angular.module('mainApp', ['ui.router', 'mainAppFilter', 'mainAppFactory', 'ngResource']);
-    
-})();
+    var module = angular.module('mainApp', ['ui.router', 'mainAppFilter', 'mainAppFactory', 'ngResource']);
+
+    return module;
+
+});
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+require.config({
+
+    baseUrl : './js/app',
+    paths   : {
+        'angular'     : 'lib/angular',
+        'ngResource'  : 'lib/angular-resource',
+        'uiRouter'    : 'lib/angular-ui-router',
+        'jQuery'      : 'lib/jquery-1.7.2'
+    },
+    shim    : {
+        'jQuery'     : {
+            'exports' : 'jQuery'
+        },
+        'angular'    : {
+            'exports' : 'angular'
+        },
+        'ngResource' : {
+            'exports' : 'ngResource',
+            'deps'    : ['angular']
+        },
+        'uiRouter' : {
+            'exports' : 'uiRouter',
+            'deps'    : ['angular']
+        }
+    }
+
+});
